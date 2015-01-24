4d23ed1c382bd46d17d3607e06c5f62dfd5bc811
Tiof <Tiof@yandex.ru>
2015-01-24 17:50:05 +0300

    Создал сцену I Am Axe

---
+++ Assets/Scenes/IAmAxe/AxeStrike.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AxeStrike : MonoBehaviour 
+{
+
+	public float strength;
+	public Transform progressbar;
+	public GameObject axe;
+
+	void Start () 
+	{
+		strength = 1;
+	}
+
+	void Update () 
+	{
+		progressbar.localScale = new Vector3(strength,1,1);
+
+	}
+}
