f896ecd553cd4830fd7e685e7c0c17b4e7a1ba02
Alex <busarovalex@yandex.ru>
2015-01-24 15:59:24 +0400

    Сериализация в текст, скрипты движения ног, рандомного движения

---
+++ Assets/Elements/LegsScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LegsScript : MonoBehaviour {
+
+	public GameObject leftLeg;
+	public GameObject rightLeg;
+
+	private bool go = false;
+	private float time;
+
+	public void Go() {
+		go = true;
+		time = 0;
+	}
+
+	public void Stop() {
+		go = false;
+		time = 0;
+		leftLeg.transform.localPosition = new Vector3 (-0.17f, 0.05f * Mathf.Sin (0), 0);
+		rightLeg.transform.localPosition = new Vector3 (0.17f, 0.05f * Mathf.Sin (0), 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (go) {
+			time -= Time.deltaTime * 12;
+			leftLeg.transform.localPosition = new Vector3 (-0.17f, 0.05f * Mathf.Sin (time), 0);
+			rightLeg.transform.localPosition = new Vector3 (0.17f, 0.05f * Mathf.Sin (time + 3.14f), 0);
+		}
+	}
+}
---
+++ Assets/Elements/SmallEnemy/RandomMovementScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMovementScript : MonoBehaviour {
+	private const float minForce = 10f;
+
+	public LegsScript legsScript;
+
+	[Range( 1f, 10f )]
+	public float movementTimePeriod = 1f;
+
+	[Range( 10f, 1000f )]
+	public float movementForce = 10f;
+
+	[Range( 0.1f, 1f )]
+	public float legMovementTime = 0.1f;
+
+	private float movementTime = 0;
+	private bool go = true;
+	
+	private float legTime = 0;
+	private bool legGo = false;
+	// Use this for initialization
+	void Start () {
+		StartRandomMovement ();
+	}
+
+	public void StartRandomMovement() {
+		go = true;
+		movementTime = 0f;
+	}
+
+	public void StopRandomMovement() {
+		legsScript.Stop ();
+		go = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (go) {
+			movementTime += Time.deltaTime;
+			if (movementTime > movementTimePeriod) {
+				movementTime = 0;
+				legGo = true;
+				legTime = 0;
+				legsScript.Go();
+				Push();
+			}
+
+			if (legGo) {
+				legTime += Time.deltaTime;
+				if (legTime > legMovementTime) {
+					legGo = false;
+					legsScript.Stop ();
+				}
+			}
+		}
+	}
+
+	private void Push() {
+		Vector2 force = new Vector2 (Random.Range (-movementForce, movementForce),
+		                             Random.Range (-movementForce, movementForce));
+		rigidbody2D.AddForce (force);
+	}
+}
