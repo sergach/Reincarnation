86a46e694ba908ff0f56e443c6d0ab6691a48c3d
Shafikova Sofiya <shafikova.sofiya@gmail.com>
2015-01-25 15:48:29 +0400

    Звуки на сцене I am drink

---
+++ Assets/Scenes/IAmPoison/IAmPoisonScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IAmPoisonScript : MonoBehaviour {
+
+	public float time;
+
+
+	void Start () {
+	}
+	
+
+	void Update () {
+
+		time -= Time.deltaTime;
+		
+		if (time < 0) 
+		{
+			Application.LoadLevel("Reincarnation");
+		}
+	
+	}
+}
