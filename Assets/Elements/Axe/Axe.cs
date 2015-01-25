5284588d270856d640746fbb9348f8620382447b
Alex <busarovalex@yandex.ru>
2015-01-25 17:35:11 +0400

    licence

--- Assets/Elements/Axe/Axe.cs
+++ Assets/Elements/Axe/Axe.cs
@@ -1,4 +1,10 @@
-﻿using UnityEngine;
+﻿/*
+ * This work created by Cucumber Studios is licensed under the 
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. 
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or 
+ * send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ * */
+using UnityEngine;
 using System.Collections;
 
 public class Axe : MonoBehaviour 
