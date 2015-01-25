513028d02482612a9180476be589551a1b2632e4
Alex <busarovalex@yandex.ru>
2015-01-25 14:53:54 +0400

    Игровая овца и ее убийство топором

--- Assets/Elements/Axe/GoldenAxeItem.cs
+++ Assets/Elements/Axe/GoldenAxeItem.cs
@@ -6,7 +6,7 @@ public class GoldenAxeItem : MonoBehaviour
 
 	void OnTriggerEnter2D(Collider2D otherColl)
 	{
-		SheepScript sheep = otherColl.gameObject.GetComponent<SheepScript>();
+		PlayableSheepScript sheep = otherColl.gameObject.GetComponent<PlayableSheepScript>();
 		Hero hero = otherColl.gameObject.GetComponent<Hero>();
 		if (sheep!=null) sheep.Die();
 		if (hero!=null) hero.GetGoldenAxe();
---
+++ Assets/Elements/Sheep/PlayableSheepScript.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayableSheepScript : MonoBehaviour {
+
+	public LegsScript legs;
+
+	private float time;
+
+	public float move = 1000;
+
+	[Range( 1f, 60f )]
+	public float timeBeforeAxe = 10;
+	private float axeTime = 0;
+	private float axeFlyTime = 2;
+
+	public GameObject axePref;
+	private GameObject axe = null;
+
+	private float deathTime = 0.5f;
+	private bool dead = false;
+
+	void Start () {
+		time = 0f;	
+	}
+	
+
+	void Update () {
+		if (!dead) {
+			bool go = false;
+
+			if (Input.GetKey (KeyCode.W)) {
+					rigidbody2D.AddForce (move * new Vector2 (0, 1) * Time.deltaTime);
+					go = true;
+			}
+			if (Input.GetKey (KeyCode.A)) {
+					rigidbody2D.AddForce (move * new Vector2 (-1, 0) * Time.deltaTime);
+					go = true;
+			}
+			if (Input.GetKey (KeyCode.S)) {
+					rigidbody2D.AddForce (move * new Vector2 (0, -1) * Time.deltaTime);
+					go = true;
+			}
+			if (Input.GetKey (KeyCode.D)) {
+					rigidbody2D.AddForce (move * new Vector2 (1, 0) * Time.deltaTime);
+					go = true;
+			}
+
+			if (go) {
+					time -= Time.deltaTime * 12;		
+					legs.Go ();
+			} else
+					legs.Stop ();
+			axeTime += Time.deltaTime;
+			if (axeTime > timeBeforeAxe) {
+				if (axe == null) {
+					axe = Instantiate (axePref, transform.position + Vector3.up * 10f, Quaternion.identity) as GameObject;
+				} else {
+					if (axeTime - timeBeforeAxe < axeFlyTime) {
+						Vector3 newPos = axe.transform.position + 
+							(axeTime - timeBeforeAxe) / axeFlyTime * (transform.position - axe.transform.position); 
+						axe.transform.position = newPos;
+					}
+				}
+
+			}
+		} else {
+			if (time < deathTime) {
+				time += Time.deltaTime;
+				transform.rotation = new Quaternion(0f, 0f, time / deathTime * 3.14f, 0f);
+			}
+		}
+	}
+
+	public void Die()
+	{
+		dead = true;
+		time = 0f;
+		legs.Stop ();
+		//rigidbody2D.
+	}
+}
