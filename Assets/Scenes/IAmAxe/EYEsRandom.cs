465fbce0cb0fed1969339044009357f64adf3fe6
Shafikova Sofiya <shafikova.sofiya@gmail.com>
2015-01-25 15:12:23 +0400

    I am Poison DONE!

--- Assets/Scenes/IAmAxe/EYEsRandom.cs
+++ Assets/Scenes/IAmAxe/EYEsRandom.cs
@@ -6,14 +6,13 @@ public class EYEsRandom : MonoBehaviour {
 	private float time;
 	public float delay;
 
-	// Use this for initialization
 	void Start () {
 		time = 0f;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		time -= Time.deltaTime;
+
 		if (time < 0) 
 		{
 			transform.localPosition = new Vector3(-transform.localPosition.x,transform.localPosition.y,transform.localPosition.z);
